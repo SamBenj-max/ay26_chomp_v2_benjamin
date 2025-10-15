90d83a0af2896f609bb6d25fcf7b4882f61b189a
SamBenj-max <stbenjamin999@gmail.com>
2025-10-15 08:36:06 -0400

    made a background

--- game.py
+++ game.py
@@ -2,9 +2,45 @@ import pygame
 
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+
+
+# screen properties
+
+WIDTH = 1280
+HEIGHT = 720
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 running = True
+
+water_tile_lo = 'pattern_56.png'
+water_tile_surface = pygame.image.load(water_tile_lo)
+
+tile_width = water_tile_surface.get_width()
+tile_height = water_tile_surface.get_height()
+
+
+
+background = pygame.Surface((WIDTH,HEIGHT))
+
+for x in range(0,WIDTH,tile_width):
+    for y in range(0,HEIGHT,tile_height):
+        background.blit(water_tile_surface,(x,y))
+
+
+y_sand = HEIGHT - tile_height
+#for x in range(0,WIDTH,tile_height):
+
+
+
+
+
+
+
+
+screen.blit(background,(0,0))
+
+
 
 while running:
     # poll for events
@@ -14,7 +50,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    #screen.fill("purple")
 
     # RENDER YOUR GAME HERE
 
